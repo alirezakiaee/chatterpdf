d07e984cd7a6f9e45ccda08efa2f8d212a843130
akphisher <alirezakiaee91@gmail.com>
2024-10-21 11:26:56 -0400

    styled the upload component

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CircleArrowDown, CircleArrowDownIcon, RocketIcon } from "lucide-react";
 import { use, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
@@ -15,14 +16,24 @@ function FileUploader() {
     <div className="flex flex-col gap-4 items-center max-w-7xl mx-auto">
       <div
         {...getRootProps()}
-        className={`flex items-center justify-center p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-400 rounded-xl h-96 text-indigo-500`}
+        className={`flex items-center justify-center p-10 border-2 border-dashed mt-10 w-[90%] border-indigo-400 rounded-xl h-96 text-indigo-500 ${
+          isFocused || isDragActive ? "bg-indigo-100" : "bg-indigo-100"
+        }`}
       >
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag and drop some files here, or click to select files</p>
-        )}
+        <div className="flex flex-col items-center">
+          {isDragActive ? (
+            <>
+              <RocketIcon className="w-20 h-20 animate-ping" />
+              <p>Drop the files here ...</p>
+            </>
+          ) : (
+            <>
+              <CircleArrowDownIcon className="w-20 h-20 animate-bounce" />
+              <p>Drag and drop some files here, or click to select files</p>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
